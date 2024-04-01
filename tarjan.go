21461aef78a7a1840592348ab068442b83f75b5a
rschio <rodrigoschio3@gmail.com>
2024-04-01 13:41:29 -0400

    tarjan make more readable

--- tarjan.go
+++ tarjan.go
@@ -13,7 +13,7 @@ func StrongComponents[T any](g Graph[T]) [][]int {
 	stk.Push(n)
 	low[n] = 0
 
-	s := &tarjanS[T]{
+	s := &scc[T]{
 		g:          g,
 		stk:        stk,
 		low:        low,
@@ -21,29 +21,14 @@ func StrongComponents[T any](g Graph[T]) [][]int {
 	}
 	for v := range g.Order() {
 		if s.unvisited(v) {
-			dfsR(s, v)
+			s.dfsR(v)
 		}
 	}
 
 	return s.components
 }
 
-func dfsR[T any](s *tarjanS[T], v int) {
-	s.previsit(v)
-	scan(s, v)
-	s.postvisit(v)
-}
-
-func scan[T any](s *tarjanS[T], v int) {
-	for w, _ := range s.g.EdgesFrom(v) {
-		if s.unvisited(w) {
-			dfsR(s, w)
-		}
-		s.retreat(v, w)
-	}
-}
-
-type tarjanS[T any] struct {
+type scc[T any] struct {
 	g          Graph[T]
 	stk        *stack[int]
 	low        []int
@@ -51,36 +36,48 @@ type tarjanS[T any] struct {
 	time       int
 }
 
-func (s *tarjanS[T]) unvisited(v int) bool {
+func (s *scc[_]) dfsR(v int) {
+	s.previsit(v)
+	s.scan(v)
+	s.postvisit(v)
+}
+
+func (s *scc[_]) scan(v int) {
+	for w, _ := range s.g.EdgesFrom(v) {
+		if s.unvisited(w) {
+			s.dfsR(w)
+		}
+		s.retreat(v, w)
+	}
+}
+
+func (s *scc[_]) unvisited(v int) bool {
 	return s.low[v] == 0
 }
 
-func (s *tarjanS[T]) leader(v int) bool {
+func (s *scc[_]) leader(v int) bool {
 	// Encoding trick. The leader bit is encoded
 	// as the negation of least significative bit.
 	return s.low[v]&1 == 0
 }
 
-func (s *tarjanS[T]) previsit(v int) {
+func (s *scc[_]) previsit(v int) {
 	s.time = s.time + 2
 	s.low[v] = s.time
 }
 
-func (s *tarjanS[T]) retreat(v, w int) {
+func (s *scc[_]) retreat(v, w int) {
 	if s.low[w] < s.low[v] {
 		s.low[v] = s.low[w] | 1
 	}
 }
 
-func (s *tarjanS[T]) postvisit(v int) {
-	if s.leader(v) {
-		s.extractComponent(v)
-	} else {
+func (s *scc[_]) postvisit(v int) {
+	if !s.leader(v) {
 		s.stk.Push(v)
+		return
 	}
-}
 
-func (s *tarjanS[T]) extractComponent(v int) {
 	var comp []int
 	for s.low[s.stk.Top()] >= s.low[v] {
 		x := s.stk.Pop()
