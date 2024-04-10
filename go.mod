bf876f652692a346dbd4d7de1b1ab42642c406e7
rschio <rodrigoschio3@gmail.com>
2024-04-10 16:17:53 -0400

    revert fancy tests
    The fancy tests are cool but is too hard and complex to read the inputs
    into Go values.
    The goal of creating a suite of tests that read scripts is to make my
    life easier, not harder. Parse simple files into Go values requires to
    use a lot o reflection and hard to write, read and maintain code.
    Maybe in the future I accept some script language, but using a external
    package to read the input.
    Grafo should focus on Graphs.
    
    Revert "move bellman ford tests to a file"
    
    This reverts commit df05524463506b7ac7e5076bafa5ab6340b7bfac.
    
    Revert "add testutil to read graph scripts"
    
    This reverts commit ae2da9da3fb7f159f9983ac45d0195da0c6b2709.
    
    Revert "change strong_test to use txtar"
    
    This reverts commit 871e48e4cc94c1e56d5e3ffce2bdbb5eeb86ec36.
    
    revert all testutil
    
    Revert "make strong test simpler"
    
    This reverts commit 8490af8c2ccdc31b9c349f101ad4d3bc69d43fb9.

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.0
 require (
 	github.com/google/go-cmp v0.6.0
 	github.com/rschio/graph v0.0.0-20221021025812-7f9d32b21e20
-	golang.org/x/tools v0.20.0
 )
 
 require github.com/yourbasic/graph v0.0.0-20210606180040-8ecfec1c2869 // indirect
--- strong_test.go
+++ strong_test.go
@@ -1,41 +1,33 @@
 package grafo
 
 import (
-	"bytes"
 	"cmp"
 	"iter"
-	"path/filepath"
 	"slices"
 	"testing"
 
 	gcmp "github.com/google/go-cmp/cmp"
-	"github.com/rschio/grafo/internal/testutil"
 	"github.com/rschio/graph"
-	"golang.org/x/tools/txtar"
 )
 
 func TestStrongComponents(t *testing.T) {
-	archive, err := txtar.ParseFile(filepath.Join("testdata", "strong.txtar"))
-	if err != nil {
-		t.Fatal(err)
-	}
+	g := NewMutable[int64](6)
+	g.Add(0, 1, 1)
+	g.Add(1, 2, 1)
+	g.Add(1, 3, 1)
+	g.Add(1, 4, 1)
+	g.Add(2, 0, 1)
+	g.Add(2, 4, 1)
+	g.Add(3, 5, 1)
+	g.Add(4, 5, 1)
+	g.Add(5, 4, 1)
 
-	for _, f := range archive.Files {
-		t.Run(f.Name, func(t *testing.T) {
-			g, err := testutil.ParseGraph[int64](bytes.NewReader(f.Data))
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			comps1 := StrongComponents(g)
-			comps2 := graph.StrongComponents(g)
-
-			sortComponents(comps1)
-			sortComponents(comps2)
-			if !gcmp.Equal(comps1, comps2) {
-				t.Errorf("\ngot %v\nwant%v\n", comps1, comps2)
-			}
-		})
+	comps1 := StrongComponents(g)
+	comps2 := graph.StrongComponents(toIterator(g))
+	sortComponents(comps1)
+	sortComponents(comps2)
+	if !gcmp.Equal(comps1, comps2) {
+		t.Errorf("\ngot %v\nwant%v\n", comps1, comps2)
 	}
 }
 
@@ -55,6 +47,16 @@ func sortComponents(comps [][]int) {
 		// a and b have at least one element.
 		return cmp.Compare(a[0], b[0])
 	})
+}
+
+func toIterator[T ~int | ~int64](g Graph[T]) *graph.Mutable {
+	h := graph.New(g.Order())
+	for v := range g.Order() {
+		for w, weight := range g.EdgesFrom(v) {
+			h.AddCost(v, w, int64(weight))
+		}
+	}
+	return h
 }
 
 type line int
