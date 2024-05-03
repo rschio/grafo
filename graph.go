393de8fb6ac64c01f5dfd0b28ef9dad11df856d0
rschio <rodrigoschio3@gmail.com>
2024-05-03 13:00:52 -0400

    add Graph doc

--- graph.go
+++ graph.go
@@ -6,8 +6,19 @@ import (
 	"reflect"
 )
 
+// Graph describes a directed weighted graph.
+//
+// The Graph interface can be used to describe both oridinary graphs and
+// multigraphs.
 type Graph[T any] interface {
+	// Order returns the number of vertices in a graph.
 	Order() int
+
+	// EdgesFrom returns an iterator that iterates over the outgoing edges of v.
+	// Each iteration returns w and weight of an edge v-[weight]->w, weight is
+	// the weight of the edge.
+	//
+	// The iteration may occur in any order, and the order may vary.
 	EdgesFrom(v int) iter.Seq2[int, T]
 }
 
