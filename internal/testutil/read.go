2cf48ca406fdf0418cf36c6ca24306314483c703
rschio <rodrigoschio3@gmail.com>
2024-08-08 14:17:50 -0400

    internal/testutil: simplify read funcs

--- internal/testutil/read.go
+++ internal/testutil/read.go
@@ -1,13 +1,9 @@
 package testutil
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/rschio/grafo/internal/encoding/simple"
@@ -23,16 +19,7 @@ func ReadFile[T any](
 		return nil, err
 	}
 	defer f.Close()
-
-	g, err := simple.NewDecoder(f, parseWeight).Decode()
-	if err != nil {
-		return nil, err
-	}
-	mg, ok := g.(*multigraph.Multigraph[T])
-	if !ok {
-		return nil, fmt.Errorf("failed to case graph decoded from simple to *Multigraph")
-	}
-	return mg, nil
+	return readGraph(f, parseWeight)
 }
 
 func ReadGraph[T any](
@@ -50,60 +37,15 @@ func ReadGraph[T any](
 	return g
 }
 
-func readGraph[T any](
-	r io.Reader,
-	parseWeight func(string) (T, error),
+func readGraph[T any](r io.Reader, parseWeight func(string) (T, error),
 ) (*multigraph.Multigraph[T], error) {
-	sc := bufio.NewScanner(r)
-
-	n, err := readNumberOfVertices(sc)
+	g, err := simple.NewDecoder(r, parseWeight).Decode()
 	if err != nil {
 		return nil, err
 	}
-
-	g := multigraph.New[T](n)
-	sep := []byte(" ")
-	for sc.Scan() {
-		line := bytes.TrimSpace(sc.Bytes())
-		if len(line) == 0 {
-			continue
-		}
-		parts := bytes.Split(line, sep)
-		if len(parts) != 3 {
-			return nil, fmt.Errorf("got %d elements in one line, want 3", len(parts))
-		}
-		vv := strings.TrimSpace(string(parts[0]))
-		ww := strings.TrimSpace(string(parts[1]))
-		wt := strings.TrimSpace(string(parts[2]))
-		v, err := strconv.Atoi(vv)
-		if err != nil {
-			return nil, err
-		}
-		w, err := strconv.Atoi(ww)
-		if err != nil {
-			return nil, err
-		}
-		weight, err := parseWeight(wt)
-		if err != nil {
-			return nil, err
-		}
-		g.Add(v, w, weight)
+	mg, ok := g.(*multigraph.Multigraph[T])
+	if !ok {
+		return nil, fmt.Errorf("failed to case graph decoded from simple to *Multigraph")
 	}
-
-	if err := sc.Err(); err != nil {
-		return nil, err
-	}
-
-	return g, nil
-}
-
-func readNumberOfVertices(sc *bufio.Scanner) (int, error) {
-	if ok := sc.Scan(); !ok {
-		return 0, fmt.Errorf("failed to read number of vertices: %w", sc.Err())
-	}
-	n, err := strconv.Atoi(sc.Text())
-	if err != nil {
-		return 0, fmt.Errorf("failed to read number of vertices: %w", err)
-	}
-	return n, nil
+	return mg, nil
 }
